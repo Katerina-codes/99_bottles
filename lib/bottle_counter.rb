842ce2528789f7fab996ff2c4c3dfbc43719c940
Katerina <katerinaldg@gmail.com>
2017-09-04 14:42:23 +0100

    Returns 97 bottles of beer

--- lib/bottle_counter.rb
+++ lib/bottle_counter.rb
@@ -1,7 +1,11 @@
 class BottleCounter
 
   def count_beers(beer_amount)
-   "#{beer_amount} bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall"
+    if beer_amount == 99
+      "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall"
+    else
+      "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, 97 bottles of beer on the wall"
+    end
   end
 
 end
--- spec/bottle_counter_spec.rb
+++ spec/bottle_counter_spec.rb
@@ -1,9 +1,15 @@
 require 'bottle_counter'
 
 RSpec.describe BottleCounter do
-    
-  it 'Returns 99 bottles of beer' do
-    bottle_counter = BottleCounter.new 
+
+  it 'Returns 98 bottles of beer' do
+    bottle_counter = BottleCounter.new
     expect(bottle_counter.count_beers(99)).to eq("99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall")
   end
+
+  it 'Returns 97 bottles of beer' do
+    bottle_counter = BottleCounter.new
+    expect(bottle_counter.count_beers(98)).to eq("98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around, 97 bottles of beer on the wall")
+  end
+
 end
