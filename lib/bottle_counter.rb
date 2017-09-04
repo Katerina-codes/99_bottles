59a81e1e6a9ae51fe76cfe04c37814871a7d6335
Katerina <katerinaldg@gmail.com>
2017-09-04 17:35:09 +0100

    Made non-public methods private

--- lib/bottle_counter.rb
+++ lib/bottle_counter.rb
@@ -4,6 +4,20 @@ class BottleCounter
     @output = output
   end
 
+  def count_beers(beer_amount)
+    until beer_amount < 0
+      if beer_amount > 1
+        beer_amount = subtract_one(beer_amount)
+      elsif beer_amount == 1
+        beer_amount = one_bottle(beer_amount)
+      else
+        beer_amount = no_more_bottles(beer_amount)
+      end
+    end
+  end
+
+  private
+
   def subtract_one(beer_amount)
     @output.puts "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, #{beer_amount - 1} bottles of beer on the wall"
     beer_amount -= 1
@@ -17,18 +31,6 @@ class BottleCounter
   def no_more_bottles(beer_amount)
     @output.puts "no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
     beer_amount -= 1
-  end
-
-  def count_beers(beer_amount)
-    until beer_amount < 0
-      if beer_amount > 1
-        beer_amount = subtract_one(beer_amount)
-      elsif beer_amount == 1
-        beer_amount = one_bottle(beer_amount)
-      else
-        beer_amount = no_more_bottles(beer_amount)
-      end
-    end
   end
 
 end
