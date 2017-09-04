af120791a0101cc341ad920004e28dc51d966a84
Katerina <katerinaldg@gmail.com>
2017-09-04 17:30:04 +0100

    Extracted elsif condition into one_bottle method

--- lib/bottle_counter.rb
+++ lib/bottle_counter.rb
@@ -9,13 +9,17 @@ class BottleCounter
     beer_amount -= 1
   end
 
+  def one_bottle(beer_amount)
+    @output.puts "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
+    beer_amount -= 1
+  end
+
   def count_beers(beer_amount)
     until beer_amount < 0
       if beer_amount > 1
         beer_amount = subtract_one(beer_amount)
       elsif beer_amount == 1
-        @output.puts "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
-        beer_amount -= 1
+        beer_amount = one_bottle(beer_amount)
       else
         @output.puts "no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
         beer_amount -= 1
