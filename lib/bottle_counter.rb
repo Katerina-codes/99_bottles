7cea5c98cb67164f941b95d7c7e0064b6aba5650
Katerina <katerinaldg@gmail.com>
2017-09-04 17:22:50 +0100

    Extracted if statement condition logic into subtract_one method

--- lib/bottle_counter.rb
+++ lib/bottle_counter.rb
@@ -4,11 +4,15 @@ class BottleCounter
     @output = output
   end
 
+  def subtract_one(beer_amount)
+    @output.puts "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, #{beer_amount - 1} bottles of beer on the wall"
+    beer_amount -= 1
+  end
+
   def count_beers(beer_amount)
     until beer_amount < 0
       if beer_amount > 1
-        @output.puts "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, #{beer_amount - 1} bottles of beer on the wall"
-        beer_amount -= 1
+        beer_amount = subtract_one(beer_amount)
       elsif beer_amount == 1
         @output.puts "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
         beer_amount -= 1
