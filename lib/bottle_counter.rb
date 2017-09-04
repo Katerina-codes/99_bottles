34cf8a7250c9f44b1520f08c49ef5b0f9dfb104b
Katerina <katerinaldg@gmail.com>
2017-09-04 16:59:57 +0100

    Put the IF statement inside the until loop so full song loops and updated tests

--- lib/bottle_counter.rb
+++ lib/bottle_counter.rb
@@ -5,13 +5,15 @@ class BottleCounter
   end
 
   def count_beers(beer_amount)
-    if beer_amount == 1
-      "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
-    elsif beer_amount == 0
-      "no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-    else
-      until beer_amount == 1
+    until beer_amount < 0
+      if beer_amount > 1
         @output.puts "#{beer_amount} bottles of beer on the wall, #{beer_amount} bottles of beer. Take one down and pass it around, #{beer_amount - 1} bottles of beer on the wall"
+        beer_amount -= 1
+      elsif beer_amount == 1
+        @output.puts "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
+        beer_amount -= 1
+      else
+        @output.puts "no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
         beer_amount -= 1
       end
     end
--- spec/bottle_counter_spec.rb
+++ spec/bottle_counter_spec.rb
@@ -24,13 +24,17 @@ RSpec.describe BottleCounter do
   end
 
   it "Returns 1 bottle of beer" do
-    bottle_counter = BottleCounter.new
-    expect(bottle_counter.count_beers(1)).to eq("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+    output = StringIO.new
+    bottle_counter = BottleCounter.new(output)
+    bottle_counter.count_beers(1)
+    expect(output.string).to include("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
   end
 
   it "returns no more bottle of beer" do
-    bottle_counter = BottleCounter.new
-    expect(bottle_counter.count_beers(0)).to eq("no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+    output = StringIO.new
+    bottle_counter = BottleCounter.new(output)
+    bottle_counter.count_beers(0)
+    expect(output.string).to include("no more bottles of beer on the wall, no more  bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
   end
 
 end
