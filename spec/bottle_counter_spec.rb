3160c6ddd4665aa202e6a191a74b1e5adbe6dd67
Katerina <katerinaldg@gmail.com>
2017-09-10 11:36:40 +0100

    Corrected typo

--- spec/bottle_counter_spec.rb
+++ spec/bottle_counter_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe BottleCounter do
     expect(output.string).to include("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
   end
 
-  it "returns no more bottle of beer" do
+  it "Returns no more bottle of beer" do
     bottle_counter.count_beers(0)
     expect(output.string).to include("no more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
   end
